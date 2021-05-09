4c78e60ac2109ba77a064eaab074d66f87340baf
Васильева Марина <marivasi96@gmail.com>
2021-05-10 00:06:45 +0500

    Исправила массивы на листы

--- StoryManager/StoryLine.cs
+++ StoryManager/StoryLine.cs
@@ -6,12 +6,25 @@ using System.Threading.Tasks;
 
 namespace StoryManager
 {
+    class NewCharacter
+    {
+        Character _character;
+        string _details;
+
+    }
     class Episode
     {
         string _name;
         Link _doc;
         string _description;
+        List<NewCharacter> newCharacters;
+
         public Episode(string name)
+        {
+            _name = name;
+            newCharacters = new List<NewCharacter>();
+        }
+        public void AddCharacter(Character character)
         {
 
         }
