70c6d7794a9d598dae0fb8d00bcd12b3ab31551b
Васильева Марина <marivasi96@gmail.com>
2021-05-09 22:29:42 +0500

    StoryLine added

---
+++ StoryManager/StoryLine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoryManager
+{
+    class Episode
+    {
+        string _name;
+        Link _doc;
+        string _description;
+        public Episode(string name)
+        {
+
+        }
+    }
+    class StoryLine
+    {
+    }
+}
